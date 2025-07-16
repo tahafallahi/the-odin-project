ea1346b5e9cb5e60e057a52a83d67602efddb84b
Taha Fallahi <tahafallahi.blob@gmail.com>
2025-07-16 23:14:50 +0330

    worked hard

--- project002-rock-paper-scissors/javascript.js
+++ project002-rock-paper-scissors/javascript.js
@@ -1,106 +1,140 @@
-// Here I used commet as pseudocode
+const startButton = document.querySelector(".start-game");
+const content = document.querySelector(".content");
+const buttons = document.querySelector(".buttons");
+const board = document.querySelector(".board");
+const computerScoreElemnt = document.createElement("h1");
+const playerScoreElemnt = document.createElement("h1");
+const scoresDiv = document.createElement("div");
+const computerHandElemnt = document.createElement("h1");
+const playerHandElemnt = document.createElement("h1");
+const handDiv = document.createElement("div");
+handDiv.appendChild(computerHandElemnt);
+handDiv.appendChild(playerHandElemnt)
+handDiv.classList.toggle("scores");
 
-// call play_game
-play_game();
+const repeatButton = document.createElement("button");
+repeatButton.textContent = "Play Again.";
+repeatButton.classList.toggle("button");
+repeatButton.addEventListener("click", e => readyGame());
 
-// define play_game
-function play_game(){
-    // define player_score of type int with default value of 0
-    let player_score = 0;
-    // define computer_score of type int with default value of 0
-    let computer_score = 0
-    // for 5 times
-    for (let i = 0; i < 5; i++) {
-        // call play_round with player_score, computer_socre
-        [player_score, computer_score] = play_round(player_score, computer_score);
-    }
-    // print the scores and the winer
-    if (player_score > computer_score) {
-        console.log(`You Won The Game \n Player: ${player_score} \n Computer: ${computer_score}`)
-    } else if (player_score < computer_score) {
-        console.log(`You Lost The Game \n Player: ${player_score} \n Computer: ${computer_score}`)
-    } else {
-        console.log(`You Tied The Game \n Player: ${player_score} \n Computer: ${computer_score}`)
-    }
+
+
+startButton.addEventListener("click", readyGame);
+
+let playButtons = []
+playButtons.push(document.createElement("button"));
+playButtons.push(document.createElement("button"));
+playButtons.push(document.createElement("button"));
+playButtons[0].textContent = "Rock";
+playButtons[1].textContent = "Paper";
+playButtons[2].textContent = "Scissors";
+playButtons[0].id = "rock";
+playButtons[1].id = "paper";
+playButtons[2].id = "scissors";
+playButtons.forEach(e => e.classList.toggle("button"));
+
+let playerScore = 0;
+let computerScore = 0;
+
+function readyGame(){
+    startButton.remove();
+    playButtons.forEach(e => buttons.appendChild(e));
+    board.firstChild.textContent = "Choose Your Hand!"
+    let playerHand;
+    playerScore = 0;
+    computerScore = 0;
+
+    if (repeatButton) repeatButton.remove();
+    scoresDiv.classList.toggle("scores");
+    playerScoreElemnt.textContent = `Player: 0`
+    computerScoreElemnt.textContent = `Computer: 0`
+    scoresDiv.appendChild(playerScoreElemnt);
+    scoresDiv.appendChild(computerScoreElemnt);
+    board.appendChild(handDiv);
+    board.appendChild(scoresDiv);
+
+    buttons.addEventListener("click", 
+            function (event) {
+                let target = event.target;
+                switch (target.id) {
+                    case "rock":
+                        playerHand = "rock";
+                        playRound(playerHand, pickRandomHand());
+                        break;
+                    case "paper":
+                        playerHand = "paper";
+                        playRound(playerHand, pickRandomHand());
+                        break;
+                    case "scissors":
+                        playerHand = "scissors";
+                        playRound(playerHand, pickRandomHand());
+                        break;
+                }
+            }
+        )
+    
 }
 
-// define play_round with paraomets of player_score, computer_score
-function play_round(player_score, computer_score) {
-    // ask the user "Pick your hand!" and store the answer in player_hand
-    let player_hand = prompt("Pick your hand");
-    // If player hand = rock = 0 if = paper = 1 if = scissiors = 2
-    if (player_hand === "rock") {player_hand = 0}
-    else if (player_hand === "paper") {player_hand = 1} 
-    else if (player_hand === "scissors") {player_hand = 2}
-    // call pick_random_hand with no arguments, store the return value in computer_hand
-    const computer_hand = pick_random_hand()
-    // define winner with the return value of comapere_hands with arguemtns of player_hand, computer_hand
-    const winner = compare_hands(player_hand, computer_hand)
-    // IF (winner = None)
-    if (winner === 0) {
-        // call announce_winnder with arguemtns, player hand, computer_hand, resutl, player_score, computer_score
-        announce_winner(player_hand, computer_hand, player_score, computer_score, "no one")
-        // rertun player_score, computer_score
-        return [player_score, computer_score]
-    // IF ELSE (winner = "player")
+
+function playRound(playerHand, computerHand) {
+    const winner = compareHands(playerHand, computerHand)
+    if (winner === "tie") {
+        announce_winner(playerHand, computerHand, "Tied")
     } else if (winner === "player") {
-        // player_score + 1
-        player_score++;
-        // call announce_winnder with arguemtns, player hand, computer_hand, resutl, score
-        announce_winner(player_hand, computer_hand, player_score, computer_score, "Won");
-        // rertun player_score, computer_score
-        return [player_score, computer_score];
-    // ELSE (winner = "computer")
+        playerScore++;
+        announce_winner(playerHand, computerHand, "Won");
     } else {
-        // computer_score + 1
-        computer_score++;
-        // call announce_winnder with arguemtns, player hand, computer_hand, resutl, score
-        announce_winner(player_hand, computer_hand, player_score, computer_score, "Lost");
-        // rertun player_score, computer_score
-        return [player_score, computer_score];
+        computerScore++;
+        announce_winner(playerHand, computerHand, "Lost");
     }
 }
 
-// define compare_hands with parameters of player_hand and computer_hand
-function compare_hands(player_hand, computer_hand) {
-    // IF  player_hand = computer_hand
+function compareHands(player_hand, computer_hand) {
     if (player_hand === computer_hand) {
-        // return 0
-        return 0;
-    // IF plyer_hand = 0 && coputer_hand = 2
-    } else if(player_hand === 0 && computer_hand === 2) {
-        // return player
+        return "tie";
+    } else if(player_hand === "rock" && computer_hand === "scissors") {
         return "player";
-    // IF plyer_hand = 1 && coputer_hand = 0
-    } else if (player_hand === 1 && computer_hand === 0) {
-        // return player
+    } else if (player_hand === "paper" && computer_hand === "rock") {
         return "player";
-    // IF plyer_hand = 2 && coputer_hand = 1
-    } else if (player_hand === 2 && computer_hand === 1) {
-        // return player
+    } else if (player_hand === "scissors" && computer_hand === "paper") {
         return "player";
     }
-    // reutnr computer
     return "computer"
 }
 
 
-// define pick_random_hand
-function pick_random_hand() {
-    // rerturn a random number from 0 to 2
-    return Math.floor(Math.random() * 10) % 3
+function pickRandomHand() {
+    const hand = Math.floor(Math.random() * 10) % 3
+    switch (hand) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
 }
 
-// define announce winner
-function announce_winner(player_hand, computer_hand, player_score, computer_score, result) {
-    // print "computer hands:" + computer_hand + "\n Your hand:" + player_hand + "\nYou." + result + "\nScore:" + score
-    console.log(computer_hand)
-    if (computer_hand === 0) {computer_hand = "rock"}
-    else if (computer_hand === 1) {computer_hand = "paper"} 
-    else if (computer_hand === 2) {computer_hand = "scissors"}
-    if (player_hand === 0) {player_hand = "rock"}
-    else if (player_hand === 1) {player_hand = "paper"} 
-    else if (player_hand === 2) {player_hand = "scissors"}
-    console.log (`Computer hand: ${computer_hand} \n Your hand: ${player_hand} \n You ${result} \n Score: player ${player_score}, computer ${computer_score}`)
+function announce_winner(playerHand, computerHand, result) {
+    console.log (`Computer: ${computerHand} You: ${playerHand} \n You ${result} \n Score: player ${playerScore}, computer ${computerScore}`)
+    playerScoreElemnt.textContent = `Player: ${playerScore}`
+    computerScoreElemnt.textContent = `Computer: ${computerScore}`
+    computerHandElemnt.textContent = `Computer: ${computerHand}`
+    playerHandElemnt.textContent = `Player: ${playerHand}`
+    board.firstChild.textContent = `You ${result}!`
+    
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore > computerScore) {
+            board.firstChild.textContent = `You Won The Game!`
+        } else if (playerScore < computerScore) {
+            board.firstChild.textContent = `You Won The Game!`
+        } else {
+            board.firstChild.textContent = `You Won The Game!`        
+        }
+        // play agains doesn't work....
+        playButtons.forEach(e => e.remove());
+        buttons.appendChild(repeatButton);
+    }
+    
 }
 
